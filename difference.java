b97dd9d9e65deb90d5ab592fe91a5b2c3cda148f
Purnashree <97431127+PurnashreeTusharGawade@users.noreply.github.com>
2023-08-20 22:04:02 +0530

    Add files via upload

---
+++ difference.java
@@ -0,0 +1,25 @@
+
+
+public class difference {
+	public static void sortarr(int a[]) {
+		for(int i=0;i<a.length;i++) {
+			for(int j=i+1;j<a.length;j++) {
+				if(a[j]<a[i]) {
+					int temp=a[i];
+					a[i]=a[j];
+					a[j]=temp;
+				}
+			}
+		}
+	}
+	public static void main(String[] args) {
+		int arr[]= {7, 9, 5, 6, 3, 2};
+		sortarr(arr);
+		int a=arr[arr.length-1];
+		int b=arr[arr.length-2];
+		System.out.print(a-b);
+	}
+}
+
+
+
